47d56eda81400535410ff0d0c02eab092e6cc140
Fernanda <40240913+Fernananas@users.noreply.github.com>
2022-05-19 12:07:32 +0200

    Delete ERN_implementation/ERN_Genturis/uploadFileHomepage directory
    on request ERN genturis, remove information Github

--- ERN_implementation/ERN_Genturis/uploadFileHomepage/upload_leaflet.sh
+++
@@ -1,1 +0,0 @@
-curl -u "admin" -X POST https://genturis-registry.eu/api/files -H 'Content-Type: application/pdf'  -H 'x-molgenis-filename: Genturis_leaflet' --data-binary @Leaflet_GENTURIS-Registry_final2.pdf
